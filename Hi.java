6b237e22f5d3284fa9e5fe5c68c88245ab8cad02
Histra <1497058369@qq.com>
2020-11-10 15:01:25 +0800

    update

---
+++ Hi.java
@@ -0,0 +1,5 @@
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("\nHi, the project belongs to Histranger\nAnd the Jenkins is OK\n");
+    }
+}
---
+++ make.sh
@@ -0,0 +1,4 @@
+ls
+javac Hi.java
+java Hi
+echo "Mission Succeed!\nBy Histranger. Thank you."
